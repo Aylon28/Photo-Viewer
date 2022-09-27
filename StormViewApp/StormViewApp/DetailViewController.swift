3419f0c21ea8e43074cd7640b47557906f9f27ee
Aylon <48117029+Aylon28@users.noreply.github.com>
2022-09-27 11:34:38 +0300

    Add files via upload

---
+++ StormViewApp/StormViewApp/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  StormViewApp
+//
+//  Created by Ilona Punya on 26.09.22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var selectedImage: String?
+    var titleNumber: (number: Int, of: Int)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let titleText = titleNumber {
+            title = "Picture \(titleText.number) of \(titleText.of)"
+        } else {
+            title = selectedImage
+        }
+        
+        navigationItem.largeTitleDisplayMode = .never
+
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+
+}
---
+++ StormViewApp/StormViewApp/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  StormViewApp
+//
+//  Created by Ilona Punya on 15.09.22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("IMG") {
+                pictures.append(item)
+            }
+        }
+        
+        pictures.sort { $0 < $1 }
+    }
+
+}
+
+extension ViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "pic", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            vc.titleNumber = (number: indexPath.row + 1, of: pictures.count)
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+}
